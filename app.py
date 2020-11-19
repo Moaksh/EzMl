e4feac03900d7deb2d0cf3c3fc606b9981c757a2
tijilchhabra1729 <60564583+tijilchhabra1729@users.noreply.github.com>
2020-11-19 11:14:37 +0530

    Update app.py

--- app.py
+++ app.py
@@ -97,7 +97,7 @@ def logistic():
 @login_required
 def knn():
     mylist = ['import pandas as pd', 'import numpy as np',
-              'from sklearn.linear_model import LogisticRegression', 'from sklearn.preprocessing import StandardScaler',
+              'from sklearn.preprocessing import StandardScaler',
               'from sklearn.neighbors import KNeighborsClassifier']
     numeric_column = []
     df_test = pd.read_csv('Tool/static/csvs/' +
