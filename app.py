3a36689c8385a76ab3ec6ef57815b321fd4f3ef2
arnav0103 <66205884+arnav0103@users.noreply.github.com>
2020-11-19 15:40:41 +0530

    duna

--- app.py
+++ app.py
@@ -61,93 +61,8 @@ def linear():
 @app.route('/ytakelinear' , methods = ['GET' , 'POST'])
 @login_required
 def ylinear():
-    numeric_column = []
-    df_train = pd.read_csv('Tool/static/csvs/' +
-                           current_user.username + 'train' + '.csv')
-    for i in df_train.columns:
-        if type(df_train[i][0]) == str:
-            continue
-        else:
-            numeric_column.append(i)
-    if request.method == 'POST':
-        y = request.form.get("y_column")
-        print(y)
-        return redirect(url_for('linear_predict' , y = y))
-    return render_template('linearpredicty.htm' , numeric_column = numeric_column)
-
-@app.route('/linearpredict/<y>' , methods = ['GET' , 'POST'])
-@login_required
-def linear_predict(y):
-    numeric_column = []
-    X_test = []
-    df_train = pd.read_csv('Tool/static/csvs/' + current_user.username + 'train' + '.csv')
-    for i in df_train.columns:
-        if type(df_train[i][0]) == str:
-            continue
-        else:
-            numeric_column.append(i)
-    numeric_column.remove(y)
-    if request.method == 'POST':
-        X_train = df_train[numeric_column]
-        y_train = df_train[y]
-        for j in request.form.getlist('x_cases'):
-            X_test.append(int(j))
-        X_test = pd.DataFrame([X_test],index=[0],columns=numeric_column)
-        lm = LinearRegression()
-        lm.fit(X_train , y_train)
-        predictions = lm.predict(X_test)
-        flash(predictions)
-    return render_template('linear_predict.htm' , numeric_column = numeric_column )
-
-# logistic regression
-
-
-@app.route('/dashboard2', methods=['GET', 'POST'])
-@login_required
-def logistic():
-    try:
-        mylist = ['import pandas as pd', 'import numpy as np',
-                  'from sklearn.linear_model import LogisticRegression']
+    if current_user.membership == 'premium':
         numeric_column = []
-        df_test = pd.read_csv('Tool/static/csvs/' +
-                              current_user.username + 'test' + 'logic'+ '.csv')
-        df_train = pd.read_csv('Tool/static/csvs/' +
-                               current_user.username + 'train' +'logic'+ '.csv')
-        for i in df_train.columns:
-            if type(df_train[i][0]) == str:
-                continue
-            else:
-                numeric_column.append(i)
-        if request.method == 'POST':
-            train_csv_name = request.form.get("train_csv")
-            test_csv_name = request.form.get("test_csv")
-            y = request.form.get("column_name")
-            numeric_column.remove(y)
-            mylist.append("df_train = pd.read_csv('" + train_csv_name + "')")
-            mylist.append("df_test = pd.read_csv('" + test_csv_name + "')")
-            mylist.append('X_train = df_train[' + str(numeric_column) + ']')
-            mylist.append("y_train = df_train['" + y + "']")
-            mylist.append('X_test = df_test[' + str(numeric_column) + ']')
-            mylist.append('lg = LogisticRegression()')
-            mylist.append('lg.fit(X_train , y_train)')
-            mylist.append('predictions = lg.predict(X_test)')
-            flash(mylist)
-    except:
-        return(redirect(url_for('upload_file_logic')))
-    return render_template("dashboard2.htm", numeric_column=numeric_column)
-
-# knn
-
-@app.route('/dashboard3', methods=['GET', 'POST'])
-@login_required
-def knn():
-    try:
-        mylist = ['import pandas as pd', 'import numpy as np',
-                  'from sklearn.preprocessing import StandardScaler',
-                  'from sklearn.neighbors import KNeighborsClassifier']
-        numeric_column = []
-        df_test = pd.read_csv('Tool/static/csvs/' +
-                              current_user.username + 'test' + '.csv')
         df_train = pd.read_csv('Tool/static/csvs/' +
                                current_user.username + 'train' + '.csv')
         for i in df_train.columns:
@@ -156,25 +71,122 @@ def knn():
             else:
                 numeric_column.append(i)
         if request.method == 'POST':
-            train_csv_name = request.form.get("train_csv")
-            test_csv_name = request.form.get("test_csv")
-            y = request.form.get("column_name")
-            numeric_column.remove(y)
-            mylist.append("df_train = pd.read_csv('" + train_csv_name + "')")
-            mylist.append("df_test = pd.read_csv('" + test_csv_name + "')")
-            mylist.append('scaler = StandardScaler()')
-            mylist.append('scaler.fit(df_train)')
-            mylist.append('scaled_features = scaler.transform(df_train)')
-            mylist.append('df_feat = pd.DataFrame(scaled_features, columns=df_train.columns)')
-            mylist.append('X_train = df_train[' + str(numeric_column) + ']')
-            mylist.append("y_train = df_train['" + y + "']")
-            mylist.append('X_test = df_test[' + str(numeric_column) + ']')
-            mylist.append('knn = KNeighborsClassifier(n_neighbors=10)')
-            mylist.append('knn.fit(X_train , y_train)')
-            mylist.append('predictions = knn.predict(X_test)')
-            flash(mylist)
-    except:
-        return(redirect(url_for('upload_file_knn')))
+            y = request.form.get("y_column")
+            print(y)
+            return redirect(url_for('linear_predict' , y = y))
+    else:
+        return redirect(url_for('get_premium'))
+    return render_template('linearpredicty.htm' , numeric_column = numeric_column)
+
+@app.route('/linearpredict/<y>' , methods = ['GET' , 'POST'])
+@login_required
+def linear_predict(y):
+    if current_user.membership == 'premium':
+        numeric_column = []
+        X_test = []
+        df_train = pd.read_csv('Tool/static/csvs/' + current_user.username + 'train' + '.csv')
+        for i in df_train.columns:
+            if type(df_train[i][0]) == str:
+                continue
+            else:
+                numeric_column.append(i)
+        numeric_column.remove(y)
+        if request.method == 'POST':
+            X_train = df_train[numeric_column]
+            y_train = df_train[y]
+            for j in request.form.getlist('x_cases'):
+                X_test.append(int(j))
+            X_test = pd.DataFrame([X_test],index=[0],columns=numeric_column)
+            lm = LinearRegression()
+            lm.fit(X_train , y_train)
+            predictions = lm.predict(X_test)
+            flash(predictions)
+        else:
+            return redirect(url_for('get_premium'))
+    return render_template('linear_predict.htm' , numeric_column = numeric_column  , y = y)
+
+# logistic regression
+
+
+@app.route('/dashboard2', methods=['GET', 'POST'])
+@login_required
+def logistic():
+    if current_user.membership == 'premium':
+        try:
+            mylist = ['import pandas as pd', 'import numpy as np',
+                      'from sklearn.linear_model import LogisticRegression']
+            numeric_column = []
+            df_test = pd.read_csv('Tool/static/csvs/' +
+                                  current_user.username + 'test' + 'logic'+ '.csv')
+            df_train = pd.read_csv('Tool/static/csvs/' +
+                                   current_user.username + 'train' +'logic'+ '.csv')
+            for i in df_train.columns:
+                if type(df_train[i][0]) == str:
+                    continue
+                else:
+                    numeric_column.append(i)
+            if request.method == 'POST':
+                train_csv_name = request.form.get("train_csv")
+                test_csv_name = request.form.get("test_csv")
+                y = request.form.get("column_name")
+                numeric_column.remove(y)
+                mylist.append("df_train = pd.read_csv('" + train_csv_name + "')")
+                mylist.append("df_test = pd.read_csv('" + test_csv_name + "')")
+                mylist.append('X_train = df_train[' + str(numeric_column) + ']')
+                mylist.append("y_train = df_train['" + y + "']")
+                mylist.append('X_test = df_test[' + str(numeric_column) + ']')
+                mylist.append('lg = LogisticRegression()')
+                mylist.append('lg.fit(X_train , y_train)')
+                mylist.append('predictions = lg.predict(X_test)')
+                flash(mylist)
+        except:
+            return redirect(url_for('upload_file_logic'))
+    else:
+        return redirect(url_for('get_premium'))
+        return render_template("dashboard2.htm", numeric_column=numeric_column)
+
+    # knn
+
+@app.route('/dashboard3', methods=['GET', 'POST'])
+@login_required
+def knn():
+    if current_user.membership == 'premium':
+        try:
+            mylist = ['import pandas as pd', 'import numpy as np',
+                      'from sklearn.preprocessing import StandardScaler',
+                      'from sklearn.neighbors import KNeighborsClassifier']
+            numeric_column = []
+            df_test = pd.read_csv('Tool/static/csvs/' +
+                                  current_user.username + 'test' + 'knn' + '.csv')
+            df_train = pd.read_csv('Tool/static/csvs/' +
+                                   current_user.username + 'train' + + 'knn' '.csv')
+            for i in df_train.columns:
+                if type(df_train[i][0]) == str:
+                    continue
+                else:
+                    numeric_column.append(i)
+            if request.method == 'POST':
+                train_csv_name = request.form.get("train_csv")
+                test_csv_name = request.form.get("test_csv")
+                y = request.form.get("column_name")
+                numeric_column.remove(y)
+                mylist.append("df_train = pd.read_csv('" + train_csv_name + "')")
+                mylist.append("df_test = pd.read_csv('" + test_csv_name + "')")
+                mylist.append('scaler = StandardScaler()')
+                mylist.append('scaler.fit(df_train)')
+                mylist.append('scaled_features = scaler.transform(df_train)')
+                mylist.append('df_feat = pd.DataFrame(scaled_features, columns=df_train.columns)')
+                mylist.append('X_train = df_train[' + str(numeric_column) + ']')
+                mylist.append("y_train = df_train['" + y + "']")
+                mylist.append('X_test = df_test[' + str(numeric_column) + ']')
+                mylist.append('knn = KNeighborsClassifier(n_neighbors=10)')
+                mylist.append('knn.fit(X_train , y_train)')
+                mylist.append('predictions = knn.predict(X_test)')
+                flash(mylist)
+        except:
+            return redirect(url_for('upload_file_knn'))
+    else:
+        return redirect(url_for('get_premium'))
     return render_template("dashboard3.htm", numeric_column=numeric_column)
 
 
@@ -291,6 +303,11 @@ def upload_file_knn():
                   current_user.username + 'test' +'knn' +'.csv')
         return redirect(url_for('knn'))
     return render_template('query.htm')
+
+@app.route('/get/premium' , methods = ['GET' , 'POST'])
+@login_required
+def get_premium():
+    return render_template('premium_get.htm')
 
 if __name__ == '__main__':
     app.run(debug=True)
